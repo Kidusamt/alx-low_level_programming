fab58e971567f070c727d3cbca79b9ccbdaf7cbb
“Kidusamt” <kidusamt@gmail.com>
2022-06-02 16:34:21 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+/** 
+ * my function
+*/
 	int main(void)
 	{		
-		char s[] = "\"Programming is like building a multilingual puzzle";
 		
-	puts(s);
-
-	return (0);	
+		char s[] = "\"Programming is like building a multilingual puzzle";
+		puts(s);
+		return (0);	
 	}
 
