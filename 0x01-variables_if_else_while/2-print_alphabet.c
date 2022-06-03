ae541e12d3f2f7bdec79c009cb8740a93d786675
“Kidusamt” <kidusamt@gmail.com>
2022-06-03 07:53:05 -0700

    alpa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+  * Description: main - chk the last number
+  * Return: 0 after complition
+  */
+int main(void)
+{
+char a=97;
+while (a < 123)
+{
+putchar(a\n)
+	a++
+}
+return (0);
+}
+		
