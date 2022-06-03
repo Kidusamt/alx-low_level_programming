ab83056859fbb893ddd6170a262eca673ef86ca0
“Kidusamt” <kidusamt@gmail.com>
2022-06-02 17:37:22 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Description: main - use of printf
+ * Return: 0 after complition
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
