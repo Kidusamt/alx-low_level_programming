a08c2f73118b83186c67f4580c4d0ca8cc9ec888
“Kidusamt” <kidusamt@gmail.com>
2022-06-03 13:35:46 -0700

    1-9 put char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>                                                                                                                   
+/**                                                                                                                                 
+ * Description: main - print 1-0                                                                                                    
+ * Return: 0 after complition                                                                                                       
+ */                                                                                                                                 
+int main(void)                                                                                                                      
+{                                                                                                                                   
+	        int i;                                                                                                                      
+		                                                                                                                                    
+		for (i = 0; i < 10; i++)                                                                                                            
+		{                                                                                                                                   
+			        putchar("%d", i);                                                                                                            
+		}                                                                                                                                   
+		putchar("\n");                                                                                                                       
+		return (0);                                                                                                                         
+}
