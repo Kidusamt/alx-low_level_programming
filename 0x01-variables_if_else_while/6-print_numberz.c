5008a16731335a09ecd8ecb1a37eba71a5ea4c61
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-04 00:38:43 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,13 +4,14 @@
  * Return: 0 after complition                                                                                                       
  */                                                                                                                                 
 int main(void)                                                                                                                      
-{                                                                                                                                   
-	        int i;                                                                                                                      
+{
+int i == 0;                                                                                                                      
 		                                                                                                                                    
-		for (i = 0; i < 10; i++)                                                                                                            
-		{                                                                                                                                   
-			        putchar(i);                                                                                                            
-		}                                                                                                                                   
-		putchar('\n');                                                                                                                       
-		return (0);                                                                                                                         
+while (i < 10)
+{
+putchar(i);
+i==
+}
+putchar('\n');
+return (0); 
 }
