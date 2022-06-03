a52864bc424aac360fc2f7b6df1ca18065df57fa
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-04 00:43:41 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,7 +9,7 @@ int i = 0;
 		                                                                                                                                    
 while (i < 10)
 {
-putchar(i);
+putchar('i');
 i++;
 }
 putchar('\n');
