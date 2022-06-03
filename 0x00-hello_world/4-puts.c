e2443fa2ecd4fc2dcaa57c4a52a82dc2a126e521
“Kidusamt” <kidusamt@gmail.com>
2022-06-02 17:24:49 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * my function
- * prints string
- * returns 0
+ * my_function - prints string
  */
 int main(void)
 /**
  * function main
- * return: o if success
+ * Return: o if success
  */
 {
 char s[] = "\"Programming is like building a multilingual puzzle";
