deceab3c08d8e6ef7638175c10713650ebd8b001
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-04 01:59:54 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,14 @@ int main(void)
 {
 int i = 0;
 
-for (i=0; i<10; i++)
+for (i = 0; i < 10; i++)
 {
 putchar(i + '0');
   if (i < 9)
   {
   putchar(',');
   }
-putchar(' ');
+putchar('');
 }
 return (0);
 }
