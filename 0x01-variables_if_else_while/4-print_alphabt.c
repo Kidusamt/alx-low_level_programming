0ebd474b071c48e45311a4def9d88153263b4121
“Kidusamt” <kidusamt@gmail.com>
2022-06-03 13:05:27 -0700

    wiith out a nad q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+nclude<stdio.h>
+/**
+ * Description: main - small letter print wit out e and q
+ *  Return: 0 after complition
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a != e && a!= q)
+	{
+		putchar(a);
+		a++;
+	}
+putchar('\n');
+return (0);
+}
