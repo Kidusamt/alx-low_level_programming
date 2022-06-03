b28cb0405ff6ecb907550f3af86aee1b764afbe4
“Kidusamt” <kidusamt@gmail.com>
2022-06-03 15:10:55 -0700

    1-9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-/** 
+/**
  * Description: main - print 1-0
  * Return: 0 after complition
  */
 int main(void)
 {
 int i = 0;
-		                                                                                                                                    
+
 while (i < 10)
 {
 putchar(i + '0');
