1508953a584b42f56fbd5e7c493f7e7775bdc0f1
“Kidusamt” <kidusamt@gmail.com>
2022-06-03 13:55:24 -0700

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * Description: main - revers alpa
+ * Return: 0 after test complition
+ */
+int main(void)
+	char a = 'z';
+	while (a <= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+putchar('\n');
+return (0);
