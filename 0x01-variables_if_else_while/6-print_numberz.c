954258eb2d7b5bbb4ca3e6c048df80cf91978c47
“Kidusamt” <kidusamt@gmail.com>
2022-06-03 13:43:57 -0700

    1-9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,7 +9,7 @@ int main(void)
 		                                                                                                                                    
 		for (i = 0; i < 10; i++)                                                                                                            
 		{                                                                                                                                   
-			        putchar("%d", i);                                                                                                            
+			        putchar("%d", int i);                                                                                                            
 		}                                                                                                                                   
 		putchar("\n");                                                                                                                       
 		return (0);                                                                                                                         
