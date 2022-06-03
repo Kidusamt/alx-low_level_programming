6f3ca1e577bf8e394621e1067a4596c7399679e7
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-03 04:18:52 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char:",sizeof(c)" byte\n");
-        printf("Size of a int:",sizeof(i)" byte\n");
-        printf("Size of a long int:",sizeof(li)" byte\n");
-        printf("Size of a long long int:",sizeof(lli)" byte\n");
-        printf("Size of a float:",sizeof(f)" byte\n");
+	printf("Size of a char:", sizeof(c) " byte\n");
+        printf("Size of a int:", sizeof(i) " byte\n");
+        printf("Size of a long int:", sizeof(li) " byte\n");
+        printf("Size of a long long int:", sizeof(lli) " byte\n");
+        printf("Size of a float:", sizeof(f) " byte\n");
 return (0);
 }
