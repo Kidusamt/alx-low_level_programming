f232158b387927cfcf56a08c9d4d145416f22492
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-04 01:46:30 +0300

    Update 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * Description: main - print 1-0
+ * Return: 0 after complition
+ */
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+putchar(i + '0');
+putchar(', ');
+i++;
+}
+putchar('\n');
+return (0);
+}
