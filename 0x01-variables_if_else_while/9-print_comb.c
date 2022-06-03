d0b4efbf6b84ce4a65af5d38a53f1a3c22c0e142
“Kidusamt” <kidusamt@gmail.com>
2022-06-03 16:17:57 -0700

    1,2,3,....

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 /**
- * Description: main - print 0-9
- * Return: 0 after complition
- */
+ *  * Description: main - print 0-9
+ *   * Return: 0 after complition
+ *    */
 int main(void)
 {
-int i = 0;
+	int i = 0;
 
-for (i = 0; i < 10; i++)
-{
-putchar(i + '0');
-if (i < 9)
-{
-putchar(',');
-}
-putchar(' ');
-}
-putchar('\n');
-return (0);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+		}
+		putchar(' ');
+	}
+	return (0);
 }
