021452f3326bd1f5315f010bb52ec99272739fa3
“Kidusamt” <kidusamt@gmail.com>
2022-06-07 16:46:42 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"                                                                                                                               
+/**                                                                                                                                             
+ *  * Description: main - print p                                                                                                                  
+ *   * Return: 0 after complition                                                                                                                   
+ *    */                                                                                                                                             
+int main(void)                                                                                                                                  
+{
+	void print_alphabet(void)
+	{
+			char letter;
+
+				for (letter = 'a'; letter <= 'z'; letter++)
+							_putchar(letter);
+
+					_putchar('\n');
+	}
+}
