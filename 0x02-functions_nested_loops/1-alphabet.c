2af87c8b1d04101b271f0b03432a629ea16cd22a
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-08 03:02:21 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
  * Description: main - print a-z 
  * Return: 0 after complition
  */
+void print_alphabet(void)
+{
+char l;
 
-	void print_alphabet(void)
-	{
-			char l;
-
-				for ( l = 'a'; l <= 'z'; l++)
-				{
-							_putchar(l);
-				}
-					_putchar('\n');
+for ( l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
 	}
