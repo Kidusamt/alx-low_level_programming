fb6038374c17202bec5cb9d4365f762a4654c630
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-08 05:06:30 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,2 +1,12 @@
 #include "main.h"
-
+/**
+ * print_to_98 - Prints numbers from input to 98
+ * @n: The number to start.
+ */
+void print_to_98(int n);
+for (i = n , i<=99, i++)
+{
+  _putchar('i'):
+}
+_putchar('\n')
+}
