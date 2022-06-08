574140ea49c2240a39f4df4576ecebdca09bd4cc
“Kidusamt” <kidusamt@gmail.com>
2022-06-07 17:47:14 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
-/** 
+/**
  * description: _islower-print if lower or not
- * return 1 if c is lower 0 of upper 
+ * Return: 1 if c is lower 0 of upper
+ * @c: The character to be checked
  */
 int _islower(int c)
 {
 		if (c >= 'a' && c <= 'z')
-					return (1);
-			else
-						return (0);
+			return (1);
+		else
+			return (0);
 }
