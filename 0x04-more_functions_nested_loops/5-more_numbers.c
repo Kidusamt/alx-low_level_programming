fb2429541d9992d37b88dabedc752b54d776b2fd
“Kidusamt” <kidusamt@gmail.com>
2022-06-09 12:36:54 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers - print 1-14 10x
+ * Return: void
+ */
+void more_numbers(void);
+{
+	char i, j;
+
+	for (i = '0'; i <= '10'; i++)
+	{
+		while (j = '0')
+			_putchar(j);
+	}
+	_putchar('\n');
+}
