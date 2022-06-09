1cd3defa5896ec0e015c5e49adeb5f5c0d7d80e4
“Kidusamt” <kidusamt@gmail.com>
2022-06-09 07:11:53 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description: chk upert or lower
+ * return 1 if uper
+ * return 0 if lower
+ */
+int _isupper(int c);
+{
+	if (c <= 65 && c > 97)
+		return (1);
+	else
+		return (0);
+
+}
