6198d697ff4f54eba1761cf5b0a8f60278b8bd19
“Kidusamt” <kidusamt@gmail.com>
2022-06-09 08:17:37 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/** 
+/**
  * mul - multiply 2 int
  * @a: is first num
- * @b: is 2nd number 
- * Return:  multpil of the 2 numbers 
+ * @b: is 2nd number
+ * Return:  multpil of the 2 numbers
  */
 int mul(int a, int b)
 {
