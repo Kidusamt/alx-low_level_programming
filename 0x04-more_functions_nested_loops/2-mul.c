909d2af52a3308f2c5031799a669eba3977d7022
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-09 18:14:08 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /** 
- * mul-multiply 2 int
+ * mul - multiply 2 int
  * @a: is first num
  * @b: is 2nd number 
- * return:  multpil of the 2 numbers 
+ * Return:  multpil of the 2 numbers 
  */
 int mul(int a, int b)
 {
-	int c;
-
 	return (a * b);
 }
