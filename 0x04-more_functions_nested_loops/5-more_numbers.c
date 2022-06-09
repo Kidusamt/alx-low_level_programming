dcc8462a9219f7d26e7cf7f280a5c99832c35b9e
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-10 00:00:04 +0300

    Update 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - print 1-14 10x
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+
+	int j = 0;
+
+	while (j <= 14)
+	{
+		printf("%d", j);
+		j++;
+	}
+
+	i++;
+
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
