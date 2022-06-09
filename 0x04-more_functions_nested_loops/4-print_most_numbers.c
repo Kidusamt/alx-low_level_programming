93c07742908ac2833bfc7092680239be50655750
“Kidusamt” <kidusamt@gmail.com>
2022-06-09 12:22:40 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers: print num from 0-9 but not 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i = '2' || i = '4')
+			continue;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
