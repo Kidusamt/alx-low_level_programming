b768351295c16b22cf02bfc09bfdda3ab5563d38
“Kidusamt” <kidusamt@gmail.com>
2022-06-09 07:54:16 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit- chks number
+ * Return: 1 if its a number or 0 if not
+ * @c : to be cheked
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+
+}
