8516d5ce98e9a17e38db9d46263741efaf5dc897
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-09 18:11:17 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,14 +3,11 @@
  * mul-multiply 2 int
  * @a: is first num
  * @b: is 2nd number 
- * return: c multpil of the 2 numbers 
+ * return:  multpil of the 2 numbers 
  */
 int mul(int a, int b)
 {
 	int c;
 
-	c = a * b;
-	_putchar ('c');
-	
-return (0);
+	return (a * b);
 }
