708bd0ab2ada89596187cf0c5e7402f7dda72cc5
“Kidusamt” <kidusamt@gmail.com>
2022-06-13 17:58:50 -0700

    678

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: pointer
+ */
+void puts_half(char *str)
+{
+	int i, l;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++
+	}
+	l = i;
+	if (l % 2 != 0)
+		l = (l + 1) / 2;
+	else 
+		l /= 2;
+	while (l < i)
+	{
+		l++;
+		_putchar(*(str + l - 1));
+	}
+	_putchar('\n');
+}
