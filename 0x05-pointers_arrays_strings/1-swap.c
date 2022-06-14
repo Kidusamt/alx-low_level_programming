d09d44e1f12c9976f293e31939f9f1dd25d11ebf
“Kidusamt” <kidusamt@gmail.com>
2022-06-13 17:00:48 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swapes to number 
+ * @a: 1st num
+ * @b 2nd num 
+ */
+void swap_int(int *a, int *b)
+{
+	int c, d;
+
+	c = *a;
+	d = *b;
+	*a = d;
+	*b = c;
+
+}
