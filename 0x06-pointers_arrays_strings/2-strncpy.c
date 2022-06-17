f97a02642c9ff7da42b27f6a761d24bb7bf9859e
“Kidusamt” <kidusamt@gmail.com>
2022-06-16 17:44:31 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+/**
+ * *_strncpy - copies a string
+ * @dest: 1st str
+ * @src: char
+ * @n: int
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < 0)
+	{
+		for (; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+	}
+	return (dest);
+}
