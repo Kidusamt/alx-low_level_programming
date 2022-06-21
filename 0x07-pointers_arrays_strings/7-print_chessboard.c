be45fbb0f7c12ed4eaddb476e24775704d9a0434
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-21 04:07:04 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * print_chessboard - print_chessboard
- * @a: arry pointe size 8
- */
-void print_chessboard(char (*a)[8])
+* print_chessboard - print_chessboard
+* @a: arry pointe size 8
+*/
 void print_chessboard(char (*a)[8])
 {
-		int i, j;
+int i, j;
 
-			for (i = 0; i <= 7; i++)
-					{
-								for (j = 0; j <= 7; j++)
-											{
-															_putchar(a[i][j]);
-																	}
-										_putchar('\n');
-											}
+for (i = 0; i <= 7; i++)
+{
+for (j = 0; j <= 7; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
 }
