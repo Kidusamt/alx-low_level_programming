8299f2c29bb13fff21fecdec8d444d10347358bd
“Kidusamt” <kidusamt@gmail.com>
2022-06-20 18:15:55 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,8 @@ unsigned int _strspn(char *s, char *accept)
 	for (j = 0; *(s + j); j++)
 	{
 		for (i = 0; *(accept + i); i++)
-		{										if (*(s + j) == *(accept + i))
+		{
+			if (*(s + j) == *(accept + i))
 			break;
 		}
 	if (*(accept + i) == '\0')
