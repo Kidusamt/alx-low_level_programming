a70b1d125c4e7082442b3994efb480cfc9a84586
“Kidusamt” <kidusamt@gmail.com>
2022-06-22 17:13:56 -0700

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial -  factorial of a number
+ * @n: the number
+ * Return: the factorial
+ */
+int factorial(int n)
+{
+	if (n < 0 )
+		return (-1);
+	if (n == o)
+		return (1);
+	return (n * factorial(n - 1));
+}
