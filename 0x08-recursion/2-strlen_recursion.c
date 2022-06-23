b0dca838c66efc14b04e8ead15c2a6a73ac19f10
“Kidusamt” <kidusamt@gmail.com>
2022-06-22 17:09:07 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Length of String
+ * @s: string
+ * Return: int n1
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
