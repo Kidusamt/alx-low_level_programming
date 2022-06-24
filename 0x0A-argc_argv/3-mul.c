d92be52b9d5a518f9c16389a506e7a7cd8778368
“Kidusamt” <kidusamt@gmail.com>
2022-06-24 13:54:05 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,3 +7,24 @@
 * @argv: arry.
 * Return: 0
 */
+int main(int argc, char *argv[])
+{
+		int i, j;
+
+			if (argc == 1 || argc == 2)
+					{
+								printf("Error\n");
+										return (1);
+											}
+				else
+						{
+									j = 1;
+
+											for (i = 1; i < 3; i++)
+															j *= atoi(argv[i]);
+
+													printf("%d\n", j);
+														}
+
+					return (0);
+}
