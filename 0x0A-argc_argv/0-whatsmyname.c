a77a33f4ff30e288dedd45dd6399cf7c2fa0ed73
“Kidusamt” <kidusamt@gmail.com>
2022-06-24 12:49:57 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
+ * main - prints the name 
+ * @argc: argument number
+ * @argv: arry of string of argument
+ * Return: 0 after complition
  */
 int main(int argc *argv)
 {
-	printf("%s", argv[0]);
+	printf("%s", *argv[0]);
 	printf("\n");
 	return (0);
 }
