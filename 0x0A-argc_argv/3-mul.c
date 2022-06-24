a16211d5c48a57181e856bc2e71769b941883003
“Kidusamt” <kidusamt@gmail.com>
2022-06-24 13:34:16 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  * main - prints all argument
+ *   * @argc: argument number
+ *    * @argv: arry
+ *     * Return: 0
+ *      */
+int main(int argc, char *argv[])
+{
+		int i;
+
+			for (i = 0; i < argc; i++)
+					{
+								printf("%s\n", argv[i]);
+									}
+				return (0);
+}
