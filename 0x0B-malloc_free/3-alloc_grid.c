c950b2aeeeba52a926a609916b4c4559fefde1af
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-29 03:25:32 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,44 +2,42 @@
 #include <stdlib.h>
 
 /**
- *  * alloc_grid - Entry point
- *   *@width: rows of matrix
- *    *@height: columns of string
- *     * Return: a pointer to a 2 dimensional array of integers or null
- *      */
+* alloc_grid - fun
+* @width: rows matrix
+* @height: columns of string
+* Return: a pointer to a 2 dimensional array
+*/
 int **alloc_grid(int width, int height)
 {
-		int **matrix;
-			int i;
-				int j;
-					int l;
-						int *p;
+int **matrix;
+int i, j, l;
+int *p;
 
-							if (width <= 0 || height <= 0)
-										return (NULL);
-								matrix = (int **)malloc(height * sizeof(int *));
-									if (matrix == NULL)
-												return (NULL);
-										for (i = 0; i < height; i++)
-												{
-															*(matrix + i) = (int *)malloc(width * sizeof(int));
-																	if (*(matrix + i) == NULL)
-																				{
-																								for (i = 0; i < height; i++)
-																												{
-																																	p = matrix[i];
-																																					free(p);
-																																								}
-																											free(matrix);
-																														return (NULL);
-																																}
-																		}
-											for (l = 0; l < height; l++)
-													{
-																for (j = 0; j < width; j++)
-																			{
-																							matrix[l][j] = 0;
-																									}
-																	}
-												return (matrix);
+if (width <= 0 || height <= 0)
+return (NULL);
+matrix = (int **)malloc(height * sizeof(int *));
+if (matrix == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+*(matrix + i) = (int *)malloc(width * sizeof(int));
+if (*(matrix + i) == NULL)
+{
+for (i = 0; i < height; i++)
+{
+p = matrix[i];
+free(p);
+}
+free(matrix);
+return (NULL);
+}
+}
+for (l = 0; l < height; l++)
+{
+for (j = 0; j < width; j++)
+{
+matrix[l][j] = 0;
+}
+}
+return (matrix);
 }
