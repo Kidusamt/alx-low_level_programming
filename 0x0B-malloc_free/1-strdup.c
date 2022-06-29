fafd39358f11185e8dccaae3913de9dfccf47073
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-06-29 03:23:24 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- *  * _strdup - Entry point
- *   *@str: string we need to duplicate
- *    * Return: a pointer to the duplicated string or NULL
- *     */
+*_strdup - fun
+* @str: string to be duplicate
+* Return: a pointer to the duplicated string or NULL
+*/
 char *_strdup(char *str)
 {
-		char *strnew = NULL;
-			unsigned int i;
-				int n;
+char *strnew = NULL;
+unsigned int i;
+int n;
 
-					if (str == NULL)
-								return (NULL);
-						for (n = 0; str[n] != '\0'; n++)
-									;
-							strnew = (char *)malloc(n + 1 * sizeof(char));
-								if (strnew != NULL)
-										{
-													for (i = 0; str[i] != '\0'; i++)
-																	strnew[i] = str[i];
-														} else
-																{
-																			return (NULL);
-																				}
-															strnew[i] = '\0';
-																return (strnew);
+if (str == NULL)
+return (NULL);
+for (n = 0; str[n] != '\0'; n++)
+;
+strnew = (char *)malloc(n + 1 * sizeof(char));
+if (strnew != NULL)
+{
+for (i = 0; str[i] != '\0'; i++)
+strnew[i] = str[i];
+} else
+{
+return (NULL);
+}
+strnew[i] = '\0';
+return (strnew);
 }
