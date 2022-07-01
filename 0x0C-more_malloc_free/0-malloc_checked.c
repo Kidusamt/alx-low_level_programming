1170ae4318d6703957b77e9e96dc1b57d848dbe5
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-07-02 01:53:05 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 /**
- *  * malloc_checked - Entry Point
- *   * @b: input amount
- *    * Return: pointer to new mem
- *     */
+*malloc_checked - Entry Point
+* @b: input amount
+* Return: pointer to new mem
+*/
 void *malloc_checked(unsigned int b)
 {
-		void *n;
+void *n;
 
-			n = malloc(b);
+n = malloc(b);
 
-				if (n == NULL)
-							exit(98);
-					return (n);
+if (n == NULL)
+exit(98);
+return (n);
 }
