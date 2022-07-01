437681c288a34748828beaa65723cb85e22c3f68
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-07-02 01:56:11 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,26 @@
 nclude <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 /**
- *  * _calloc - print 0s into 2D grid
- *   * @nmemb: array num
- *    * @size: size of ea array element
- *     * Return: 0
- *      */
+*_calloc - print 0s into 2D grid
+* @nmemb: array num
+* @size: size of ea array element
+* Return: 0
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-		char *arr;
-			unsigned int i;
+char *arr;
+unsigned int i;
 
-				if (nmemb == 0 || size == 0)
-							return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-					arr = malloc(nmemb * size);
-						if (arr == NULL)
-									return (NULL);
+arr = malloc(nmemb * size);
+if (arr == NULL)
+return (NULL);
 
-							for (i = 0; i < (nmemb * size); i++)
-										arr[i] = 0;
+for (i = 0; i < (nmemb * size); i++)
+arr[i] = 0;
 
-								return (arr);
+return (arr);
 }
