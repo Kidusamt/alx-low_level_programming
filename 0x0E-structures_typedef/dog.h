e1297f020bae39ada6d5f04617d3fd116b537edd
“Kidusamt” <kidusamt@gmail.com>
2022-07-04 17:08:55 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,10 +10,11 @@
 
 typedef struct dog
 {
-		char *name;
-			float age;
-				char *owner;
-} dog_t;
+char *name;
+float age;
+char *owner;
+}
+dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
