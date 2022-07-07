a915cd56220f7121a6551e6d5430d9956076ec57
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-07-07 19:41:47 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,13 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*  * print_numbers - prints integer arguments with a separator
-*   *
-*    * @separator: - thing to print between numbers
-*     * @n: - number of arguments
-*      *
-*       * Return: void
-*        */
+* print_numbers - prints integer arguments with a separator
+* @separator: - thing to print between numbers
+* @n: - number of arguments
+* Return: void
+*/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 char *sep;
