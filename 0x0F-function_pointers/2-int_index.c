5f20b4b2eed7963e33202891c6624b491d486922
“Kidusamt” <kidusamt@gmail.com>
2022-07-07 07:02:52 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,9 +10,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 if (size <= 0 || array == NULL || cmp == NULL)
 return (-1);
 int i;
-for (i=0; i<size; i++)
+for (i = 0; i < size; i++)
 {
 if (cmp(*(array + i)))
 return (i);
 }
+return (-1);
 }
