3b54d42c1b08696c7dc968a6412143d4243b9da6
“Kidusamt” <kidusamt@gmail.com>
2022-07-07 09:38:15 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
+/**
+ *  * printf_char - printfs a char from var args
+ *   *
+ *    * @list: va_list to print from
+ *     *
+ *      * Return: void
+ *       */
+void printf_char(va_list list)
+{
+		printf("%c", (char) va_arg(list, int));
+}
+
+/**
+ *  * printf_int - printfs an int from var args
+ *   *
+ *    * @list: va_list to print from
+ *     *
+ *      * Return: void
+ *       */
+void printf_int(va_list list)
+{
+		printf("%d", va_arg(list, int));
+}
+
+/**
+ *  * printf_float - printfs a float from var args
+ *   *
+ *    * @list: va_list to print from
+ *     *
+ *      * Return: void
+ *       */
+void printf_float(va_list list)
+{
+		printf("%f", (float) va_arg(list, double));
+}
+
+/**
+ *  * printf_string - printfs a string from var args
+ *   *
+ *    * @list: va_list to print from
  *
  *  * Return: void
  *   */
