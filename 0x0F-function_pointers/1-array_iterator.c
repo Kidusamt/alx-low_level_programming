3fe0ea6028d28d5de2642783166fc964f9824817
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-07-07 16:06:57 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,16 @@
-terator(int *array, size_t size, void (*action)(int))
+/**
+* array_iterator - func
+* @arry: arry
+* @size: size of array
+* @action: func pointer
+*/
+array_terator(int *array, size_t size, void (*action)(int))
 {
-		size_t i;
+size_t i;
 
-			if (array && size && action)
-					{
-							for (i = 0; i < size; i++)
-										action(*(array + i));
-								}
+if (array && size && action)
+{
+for (i = 0; i < size; i++)
+action(*(array + i));
+}
 }
