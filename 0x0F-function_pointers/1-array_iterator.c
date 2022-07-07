87467df0b7c9d5d8d28b5241cecebe9cc73b539d
“Kidusamt” <kidusamt@gmail.com>
2022-07-07 06:03:51 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,10 @@
+terator(int *array, size_t size, void (*action)(int))
+{
+		size_t i;
+
+			if (array && size && action)
+					{
+							for (i = 0; i < size; i++)
+										action(*(array + i));
+								}
+}
