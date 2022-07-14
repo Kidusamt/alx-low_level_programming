7c0c541f155a7a80a2babdc132b3fd6e83355c7d
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-07-14 19:02:02 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,13 @@
 
 void test(void) __attribute__((constructor));
 /**
- *  * test - function with constructor
- *   *
- *    * Description: print string
- *     * Return: na
- *      */
+*  * test - function with constructor
+*   *
+*    * Description: print string
+*     * Return: na
+*      */
 void test(void)
 {
-		printf("You're beat! and yet, you must allow,\n");
-			printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
