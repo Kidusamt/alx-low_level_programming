d2fece1ebe3da9316572234826e881b623fed6dd
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-07-20 16:01:55 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,21 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- *  * sum_listint - return sum of list data
- *   * @head: head node
- *    * Return: sum
- *     */
+*  * sum_listint - return sum of list data
+*   * @head: head node
+*    * Return: sum
+*/
 int sum_listint(listint_t *head)
 {
-		int sum = 0;
+int sum = 0;
 
-			if (!head)
-						return (sum);
+if (!head)
+return (sum);
 
-				while (head)
-						{
-									sum += head->n;
-											head = head->next;
-												}
-					return (sum);
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
